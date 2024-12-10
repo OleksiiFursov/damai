4ac2bca6671647d52767ea4c92424d50f6783238
OleksiiFursov <nodepro777@gmail.com>
2024-12-11 00:28:23 +0100

    first

---
+++ assets/js/main.js
@@ -0,0 +1,613 @@
+(function ($) {
+  ("use strict");
+  /*=================================
+      JS Index Here
+  ==================================*/
+  /*
+    01. On Load Function
+    02. Preloader
+    03. Mobile Menu Active
+    04. Sticky fix
+    05. Scroll To Top
+    06. Set Background Image
+    07. Global Slider
+    08. Ajax Contact Form
+    09. Magnific Popup
+    10. Filter
+    11. Popup Sidemenu   
+    12. Search Box Popup
+    13. Woocommerce Toggle
+    14. Quantity Added
+    15. Shape Mockup
+    16. Range Slider
+    17. WOW.js Animation
+    18. Hero Slider Active
+    19. Testimonial Slider
+    20. Date & Time Picker
+    21. Accordion Class Toggler
+    22. Parallax Effect
+  */
+  /*=================================
+      JS Index End
+  ==================================*/
+  /*
+
+  /*---------- 01. On Load Function ----------*/
+  $(window).on("load", function () {
+    $(".preloader").fadeOut();
+  });
+
+  /*---------- 02. Preloader ----------*/
+  if ($(".preloader").length > 0) {
+    $(".preloaderCls").each(function () {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".preloader").css("display", "none");
+      });
+    });
+  }
+
+  /*---------- 03. Mobile Menu Active ----------*/
+  $.fn.vsmobilemenu = function (options) {
+    var opt = $.extend(
+      {
+        menuToggleBtn: ".vs-menu-toggle",
+        bodyToggleClass: "vs-body-visible",
+        subMenuClass: "vs-submenu",
+        subMenuParent: "vs-item-has-children",
+        subMenuParentToggle: "vs-active",
+        meanExpandClass: "vs-mean-expand",
+        appendElement: '<span class="vs-mean-expand"></span>',
+        subMenuToggleClass: "vs-open",
+        toggleSpeed: 400,
+      },
+      options
+    );
+
+    return this.each(function () {
+      var menu = $(this); // Select menu
+
+      // Menu Show & Hide
+      function menuToggle() {
+        menu.toggleClass(opt.bodyToggleClass);
+
+        // collapse submenu on menu hide or show
+        var subMenu = "." + opt.subMenuClass;
+        $(subMenu).each(function () {
+          if ($(this).hasClass(opt.subMenuToggleClass)) {
+            $(this).removeClass(opt.subMenuToggleClass);
+            $(this).css("display", "none");
+            $(this).parent().removeClass(opt.subMenuParentToggle);
+          }
+        });
+      }
+
+      // Class Set Up for every submenu
+      menu.find("li").each(function () {
+        var submenu = $(this).find("ul");
+        submenu.addClass(opt.subMenuClass);
+        submenu.css("display", "none");
+        submenu.parent().addClass(opt.subMenuParent);
+        submenu.prev("a").append(opt.appendElement);
+        submenu.next("a").append(opt.appendElement);
+      });
+
+      // Toggle Submenu
+      function toggleDropDown($element) {
+        if ($($element).next("ul").length > 0) {
+          $($element).parent().toggleClass(opt.subMenuParentToggle);
+          $($element).next("ul").slideToggle(opt.toggleSpeed);
+          $($element).next("ul").toggleClass(opt.subMenuToggleClass);
+        } else if ($($element).prev("ul").length > 0) {
+          $($element).parent().toggleClass(opt.subMenuParentToggle);
+          $($element).prev("ul").slideToggle(opt.toggleSpeed);
+          $($element).prev("ul").toggleClass(opt.subMenuToggleClass);
+        }
+      }
+
+      // Submenu toggle Button
+      var expandToggler = "." + opt.meanExpandClass;
+      $(expandToggler).each(function () {
+        $(this).on("click", function (e) {
+          e.preventDefault();
+          toggleDropDown($(this).parent());
+        });
+      });
+
+      // Menu Show & Hide On Toggle Btn click
+      $(opt.menuToggleBtn).each(function () {
+        $(this).on("click", function () {
+          menuToggle();
+        });
+      });
+
+      // Hide Menu On out side click
+      menu.on("click", function (e) {
+        e.stopPropagation();
+        menuToggle();
+      });
+
+      // Stop Hide full menu on menu click
+      menu.find("div").on("click", function (e) {
+        e.stopPropagation();
+      });
+    });
+  };
+
+  $(".vs-menu-wrapper").vsmobilemenu();
+
+  /*---------- 04. Sticky fix ----------*/
+  var lastScrollTop = "";
+  var scrollToTopBtn = ".scrollToTop";
+
+  function stickyMenu($targetMenu, $toggleClass, $parentClass) {
+    var st = $(window).scrollTop();
+    var height = $targetMenu.css("height");
+    $targetMenu.parent().css("min-height", height);
+    if ($(window).scrollTop() > 800) {
+      $targetMenu.parent().addClass($parentClass);
+
+      if (st > lastScrollTop) {
+        $targetMenu.removeClass($toggleClass);
+      } else {
+        $targetMenu.addClass($toggleClass);
+      }
+    } else {
+      $targetMenu.parent().css("min-height", "").removeClass($parentClass);
+      $targetMenu.removeClass($toggleClass);
+    }
+    lastScrollTop = st;
+  }
+  $(window).on("scroll", function () {
+    stickyMenu($(".sticky-active"), "active", "will-sticky");
+    if ($(this).scrollTop() > 500) {
+      $(scrollToTopBtn).addClass("show");
+    } else {
+      $(scrollToTopBtn).removeClass("show");
+    }
+  });
+
+  /*---------- 06. Set Background Image ----------*/
+  $.fn.vsBgSetup = function () {
+    $(this).each(function () {
+      var src = $(this).attr("data-bg-src");
+      $(this).css("background-image", "url(" + src + ")");
+      $(this).removeAttr("data-bg-src").addClass("background-image");
+    });
+  };
+
+  $.fn.vsMaskSetup = function () {
+    $(this).each(function () {
+      var mask = $(this).attr("data-mask-src");
+      $(this).css({
+        "mask-image": "url(" + mask + ")",
+        "-webkit-mask-image": "url(" + mask + ")",
+      });
+      $(this).removeAttr("data-mask-src");
+    });
+  };
+
+  $("[data-bg-src]").vsBgSetup();
+  $("[data-mask-src]").vsMaskSetup();
+
+  /*----------- 07. Global Slider ----------*/
+  $(".vs-carousel").each(function () {
+    var asSlide = $(this);
+
+    // Collect Data
+    function d(data) {
+      return asSlide.data(data);
+    }
+
+    // Custom Arrow Button
+    var prevButton =
+        '<button type="button" class="slick-prev"><i class="' +
+        d("prev-arrow") +
+        '"></i></button>',
+      nextButton =
+        '<button type="button" class="slick-next"><i class="' +
+        d("next-arrow") +
+        '"></i></button>';
+
+    // Check for arrow wrapper
+    if (d("arrows") == true) {
+      if (!asSlide.closest(".arrow-wrap").length) {
+        asSlide.closest(".container").parent().addClass("arrow-wrap");
+      }
+    }
+
+    asSlide.slick({
+      dots: d("dots") ? true : false,
+      fade: d("fade") ? true : false,
+      arrows: d("arrows") ? true : false,
+      speed: d("speed") ? d("speed") : 1000,
+      asNavFor: d("asnavfor") ? d("asnavfor") : false,
+      autoplay: d("autoplay") == false ? false : true,
+      infinite: d("infinite") == false ? false : true,
+      slidesToShow: d("slide-show") ? d("slide-show") : 1,
+      adaptiveHeight: d("adaptive-height") ? true : false,
+      centerMode: d("center-mode") ? true : false,
+      autoplaySpeed: d("autoplay-speed") ? d("autoplay-speed") : 8000,
+      centerPadding: d("center-padding") ? d("center-padding") : "0",
+      focusOnSelect: d("focuson-select") == false ? false : true,
+      pauseOnFocus: d("pauseon-focus") ? true : false,
+      pauseOnHover: d("pauseon-hover") ? true : false,
+      variableWidth: d("variable-width") ? true : false,
+      vertical: d("vertical") ? true : false,
+      verticalSwiping: d("vertical") ? true : false,
+      prevArrow: d("prev-arrow")
+        ? prevButton
+        : '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+      nextArrow: d("next-arrow")
+        ? nextButton
+        : '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+      rtl: $("html").attr("dir") == "rtl" ? true : false,
+      responsive: [
+        {
+          breakpoint: 1600,
+          settings: {
+            arrows: d("xl-arrows") ? true : false,
+            dots: d("xl-dots") ? true : false,
+            slidesToShow: d("xl-slide-show")
+              ? d("xl-slide-show")
+              : d("slide-show"),
+            centerMode: d("xl-center-mode") ? true : false,
+            centerPadding: d("xl-center-padding")
+              ? d("xl-center-padding")
+              : "0",
+          },
+        },
+        {
+          breakpoint: 1400,
+          settings: {
+            arrows: d("ml-arrows") ? true : false,
+            dots: d("ml-dots") ? true : false,
+            slidesToShow: d("ml-slide-show")
+              ? d("ml-slide-show")
+              : d("slide-show"),
+            centerMode: d("ml-center-mode") ? true : false,
+            centerPadding: d("ml-center-padding")
+              ? d("ml-center-padding")
+              : "0",
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            arrows: d("lg-arrows") ? true : false,
+            dots: d("lg-dots") ? true : false,
+            slidesToShow: d("lg-slide-show")
+              ? d("lg-slide-show")
+              : d("slide-show"),
+            centerMode: d("lg-center-mode") ? true : false,
+            centerPadding: d("lg-center-padding")
+              ? d("lg-center-padding")
+              : "0",
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: d("md-arrows") ? true : false,
+            dots: d("md-dots") ? true : false,
+            slidesToShow: d("md-slide-show") ? d("md-slide-show") : 1,
+            centerMode: d("md-center-mode") ? true : false,
+            centerPadding: d("md-center-padding")
+              ? d("md-center-padding")
+              : "0",
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            arrows: d("sm-arrows") ? true : false,
+            dots: d("sm-dots") ? true : false,
+            slidesToShow: d("sm-slide-show") ? d("sm-slide-show") : 1,
+            centerMode: d("sm-center-mode") ? true : false,
+            centerPadding: d("sm-center-padding")
+              ? d("sm-center-padding")
+              : "0",
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            arrows: d("xs-arrows") ? true : false,
+            dots: d("xs-dots") ? true : false,
+            slidesToShow: d("xs-slide-show") ? d("xs-slide-show") : 1,
+            centerMode: d("xs-center-mode") ? true : false,
+            centerPadding: d("xs-center-padding")
+              ? d("xs-center-padding")
+              : "0",
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
+  });
+
+
+  // Function For Custom Arrow Btn
+  $.fn.slickGoNext = function () {
+    $(this).each(function () {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $($(this).data("slick-next")).slick("slickNext");
+      });
+    });
+  };
+
+  $.fn.slickGoPrev = function () {
+    $(this).each(function () {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $($(this).data("slick-prev")).slick("slickPrev");
+      });
+    });
+  };
+
+  $("[data-slick-next]").slickGoNext();
+  $("[data-slick-prev]").slickGoPrev();
+
+
+
+  /*---------- 11. Popup Sidemenu ----------*/
+  function popupSideMenu($sideMenu, $sideMunuOpen, $sideMenuCls, $toggleCls) {
+    // Sidebar Popup
+    $($sideMunuOpen).on("click", function (e) {
+      e.preventDefault();
+      $($sideMenu).addClass($toggleCls);
+    });
+    $($sideMenu).on("click", function (e) {
+      e.stopPropagation();
+      $($sideMenu).removeClass($toggleCls);
+    });
+    var sideMenuChild = $sideMenu + " > div";
+    $(sideMenuChild).on("click", function (e) {
+      e.stopPropagation();
+      $($sideMenu).addClass($toggleCls);
+    });
+    $($sideMenuCls).on("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $($sideMenu).removeClass($toggleCls);
+    });
+  }
+  popupSideMenu(
+    ".sidemenu-wrapper",
+    ".sideMenuToggler",
+    ".sideMenuCls",
+    "show"
+  );
+
+
+  /*----------- 15. Shape Mockup ----------*/
+  $.fn.shapeMockup = function () {
+    var $shape = $(this);
+    $shape.each(function () {
+      var $currentShape = $(this),
+        shapeTop = $currentShape.data("top"),
+        shapeRight = $currentShape.data("right"),
+        shapeBottom = $currentShape.data("bottom"),
+        shapeLeft = $currentShape.data("left");
+      $currentShape
+        .css({
+          top: shapeTop,
+          right: shapeRight,
+          bottom: shapeBottom,
+          left: shapeLeft,
+        })
+        .removeAttr("data-top")
+        .removeAttr("data-right")
+        .removeAttr("data-bottom")
+        .removeAttr("data-left")
+        .parent()
+        .addClass("shape-mockup-wrap");
+    });
+  };
+
+  if ($(".shape-mockup")) {
+    $(".shape-mockup").shapeMockup();
+  }
+
+  /*----------- 16. Range Slider ----------*/
+  $("#slider-range").slider({
+    range: true,
+    min: 40,
+    max: 300,
+    values: [60, 570],
+    slide: function (event, ui) {
+      $("#minAmount").text("$" + ui.values[0]);
+      $("#maxAmount").text("$" + ui.values[1]);
+    },
+  });
+  $("#minAmount").text("$" + $("#slider-range").slider("values", 0));
+  $("#maxAmount").text("$" + $("#slider-range").slider("values", 1));
+
+  /*----------- 17. WOW.js Animation ----------*/
+  var wow = new WOW({
+    boxClass: "wow", // animated element css class (default is wow)
+    animateClass: "wow-animated", // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: false, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: false, // reset animation on end (default is true)
+  });
+  wow.init();
+
+
+
+  /*----------- 18. Hero Slider Active ----------*/
+  $(".vs-hero-carousel").each(function () {
+    var vsHslide = $(this);
+
+    // Get Data From Dom
+    function d(data) {
+      return vsHslide.data(data);
+    }
+
+
+    /* Custom Thumb Navigation */
+    var customNav = '.thumb';
+    var navDom = 'data-slide-go';
+
+    vsHslide.on('sliderDidLoad', function (event, slider) { // On Slide Init
+      var currentSlide = slider.slides.current.index; // current Slide index 
+      var i = 1;
+      // Set Attribute 
+      vsHslide.find(customNav).each(function () {
+        $(this).attr(navDom, i)
+        i++
+        // On Click Thumb, Change slide
+        $(this).on('click', function (e) {
+          e.preventDefault();
+          var target = $(this).attr(navDom);
+          vsHslide.layerSlider(parseInt(target));
+        })
+      });
+      // Add class To current Thumb
+      var currentNav = customNav + '[' + navDom + '="' + currentSlide + '"';
+      $(currentNav).addClass('active');
+    }).on('slideChangeDidComplete', function (event, slider) { // On slide Change Start
+      var currentActive = slider.slides.current.index; // After change Current Index
+      var currentNav = customNav + '[' + navDom + '="' + currentActive + '"';
+      $(currentNav).addClass('active') // Add Class on current Nav
+      $(currentNav).siblings().removeClass('active');
+    });
+
+    /* Custom Responsive Option */
+    vsHslide.on('sliderWillLoad', function (event, slider) {
+      // Define Variable
+      var respLayer = jQuery(this).find('.ls-responsive'),
+        lsDesktop = 'ls-desktop',
+        lsLaptop = 'ls-laptop',
+        lsTablet = 'ls-tablet',
+        lsMobile = 'ls-mobile',
+        windowWid = jQuery(window).width(),
+        lgDevice = 1025,
+        mdDevice = 992,
+        smDevice = 767;
+
+      // Set Style on each Layer
+      respLayer.each(function () {
+        var layer = jQuery(this);
+
+        function lsd(data) {
+          return (layer.data(data) === '') ? layer.data(null) : layer.data(data);
+        }
+        // var respStyle = (windowWid < smDevice) ? ((lsd(lsMobile)) ? lsd(lsMobile) : lsd(lsTablet)) : ((windowWid < mdDevice) ? ((lsd(lsTablet)) ? lsd(lsTablet) : lsd(lsDesktop)) : lsd(lsDesktop)),
+        var respStyle = (windowWid < smDevice) ? lsd(lsMobile) : ((windowWid < mdDevice ? lsd(lsTablet) : ((windowWid < lgDevice) ? lsd(lsLaptop) : lsd(lsDesktop)))),
+          mainStyle = (layer.attr('style') !== undefined) ? layer.attr('style') : ' ';
+
+        layer.attr('style', mainStyle + respStyle);
+      });
+
+    });
+
+    vsHslide.layerSlider({
+      allowRestartOnResize: true,
+      maxRatio: d("maxratio") ? d("maxratio") : 1,
+      type: d("slidertype") ? d("slidertype") : "responsive",
+      pauseOnHover: d("pauseonhover") ? true : false,
+      navPrevNext: d("navprevnext") ? true : false,
+      hoverPrevNext: d("hoverprevnext") ? true : false,
+      hoverBottomNav: d("hoverbottomnav") ? true : false,
+      navStartStop: d("navstartstop") ? true : false,
+      navButtons: d("navbuttons") ? true : false,
+      loop: d("loop") === false ? false : true,
+      autostart: d("autostart") ? true : false,
+      height: d("height") ? d("height") : 1080,
+      responsiveUnder: d("responsiveunder") ? d("responsiveunder") : 1220,
+      layersContainer: d("container") ? d("container") : 1220,
+      showCircleTimer: d("showcircletimer") ? true : false,
+      skinsPath: "layerslider/skins/",
+      thumbnailNavigation: d("thumbnailnavigation") === false ? false : true,
+      startInViewport: false,
+      allowFullscreen: false,
+    });
+  });
+
+
+
+
+
+  /*----------- 19. Testimonial Slider ----------*/
+  $('#testis_4_1').slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    autoplay: true,
+    autoplaySpeed: 6000,
+    fade: false,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '#testis_4_2, #testis_4_3',
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('#testis_4_2').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    fade: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
+    focusOnSelect: true,
+    asNavFor: '#testis_4_1, #testis_4_3'
+  });
+
+  $('#testis_4_3').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    fade: false,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '#testis_4_2, #testis_4_1'
+  });
+
+
+
+
+
+
+
+  /*----------- 21. Accordion Class Toggler ----------*/
+  $(".accordion-button").on("click", function () {
+    let btn = $(this).closest(".accordion-item");
+    btn.toggleClass("active").siblings().removeClass("active");
+  });
+
+
+
+
+  /*---------- 22. Parallax Effect ----------*/
+  new universalParallax().init();
+
+
+  
+
+})(jQuery);
